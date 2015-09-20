a5045a02d5d70a75647a7538b8f12ffc6e84f94b
ayalgelles <ayalgelles@gmail.com>
2015-09-20 19:12:30 +0300

    decent

--- preprocess.js
+++ preprocess.js
@@ -5,7 +5,7 @@ if(!config.facebook.appId || !config.facebook.appSecret) {
     throw new Error('facebook appId and appSecret required in config.js');
 }
 
-FB.api('santakatarina2', {fields: 'About', access_token:'1616992805231820|hqB6t7IxNhehM5LToLE07M0oJ1A'}, function (res) {
+FB.api('santakatarina2', {fields: 'about,hours,name,location', access_token:'1616992805231820|hqB6t7IxNhehM5LToLE07M0oJ1A'}, function (res) {
   if(!res || res.error) {
    console.log(!res ? 'error occurred' : res.error);
    return;
